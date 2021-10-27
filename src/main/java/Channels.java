c937e4e70beb53664bd39a5773a54c32f847aa2f
Valentina Filonova <filonova2001v@gmail.com>
2021-10-27 15:57:30 +0300

    second

---
+++ src/main/java/Channels.java
@@ -0,0 +1,19 @@
+public enum Channels {
+    ANIMAl_PLANET,
+    CINEMA,
+    DISNEY,
+    TV_SHOW,
+    WINTER,
+    RAIN,
+    EUROSPORT,
+    MATCH_TV,
+    DISCOVERY_SCIENCE,
+    HISTORY,
+    PARAMOUNT_CHANNEL,
+    FOX,
+    RETRO;
+//    private int i;
+//    Channels(int i) {
+//        this.i = i;
+//    }
+}
---
+++ src/test/java/MyContractListTest.java
@@ -0,0 +1,134 @@
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyContractListTest {
+
+    private MyList<Contract> listContract = new MyContractList<>();
+
+    @BeforeEach
+    void init() {
+        System.out.println("startup");
+//        MyList<Contract> list = new MyContractList<>();
+        Person p1 = new Person("Valentina", LocalDate.of(2001, 11, 18), 'w');
+        Person p2 = new Person("Alex", LocalDate.of(2000, 2, 28), 'm');
+        MobileContract mb1 = new MobileContract(0, LocalDate.of( 2019, 11, 10),
+                LocalDate.of(2022, 11, 10), 0, p1, 100, 10, 4);
+        MobileContract mb2 = new MobileContract(1, LocalDate.of(2020, 11, 10),
+                LocalDate.of(2023, 11, 10), 1, p2, 100, 10, 4);
+        listContract.add(mb1);
+        listContract.add(mb2);
+//        listContract = list;
+//        return list;
+
+    }
+
+    @Test
+    void add() {
+        System.out.println("test add");
+        Person p2 = new Person("Alex", LocalDate.of(2000, 2, 28), 'm');
+
+        TVContract c3 = new TVContract(2, LocalDate.of(2019, 12, 10),
+                LocalDate.of(2022, 12, 10),
+                2, p2, new ArrayList<Channels>());
+
+        listContract.add(c3, 1);
+        assertEquals(3, listContract.size());
+    }
+
+
+    @Test
+    void testAdd() {
+        Person p1 = new Person("Valentina", LocalDate.of(2001, 11, 18), 'w');
+        Person p2 = new Person("Alex", LocalDate.of(2000, 2, 28), 'm');
+
+        TVContract c3 = new TVContract(2, LocalDate.of(2019, 12, 10),
+                LocalDate.of(2022, 12, 10),
+                2, p2, new ArrayList<Channels>());
+        MyList<Contract> listContract = new MyContractList();
+
+
+        listContract.add(c3, 1);
+        assertEquals(c3, listContract.get(1));
+        assertEquals(3, listContract.size());
+    }
+
+    @Test
+    void remove() {
+        Person p2 = new Person("Alex", LocalDate.of(2000, 2, 28), 'm');
+        TVContract c3 = new TVContract(2, LocalDate.of(2019, 12, 10),
+                LocalDate.of(2022, 12, 10),
+                2, p2, new ArrayList<Channels>());
+        MyList<Contract> listContract = new MyContractList();
+        listContract.add(c3);
+
+        listContract.remove(1);
+        assertEquals(2, listContract.size());
+        assertEquals(c3, listContract.get(1));
+    }
+
+    @Test
+    void removeByID() {
+        Person p1 = new Person("Valentina", LocalDate.of(2001, 11, 18), 'w');
+        Person p2 = new Person("Alex", LocalDate.of(2000, 2, 28), 'm');
+        MobileContract c1 = new MobileContract(0,LocalDate.of(2019, 11, 10),
+                LocalDate.of(2022, 11, 10), 0, p1, 100, 10, 4);
+        InternetContract c2 = new InternetContract(1, LocalDate.of(2019, 11, 10),
+                LocalDate.of(2022, 11, 10),
+                1, p1, 300);
+        TVContract c3 = new TVContract(2, LocalDate.of(2019, 12, 10),
+                LocalDate.of(2022, 12, 10),
+                2, p2, new ArrayList<Channels>());
+        MyList<Contract> listContract = new MyContractList();
+        listContract.add(c1);
+        listContract.add(c2);
+        listContract.add(c3);
+
+        listContract.removeByID(c1.getID());
+        assertEquals(2, listContract.size());
+        assertEquals(c3, listContract.get(1));
+    }
+
+    @Test
+    void get() {
+        Person p1 = new Person("Valentina", LocalDate.of(2001, 11, 18), 'w');
+        Person p2 = new Person("Alex", LocalDate.of(2000, 2, 28), 'm');
+        MobileContract c1 = new MobileContract(0,LocalDate.of(2019, 11, 10),
+                LocalDate.of(2022, 11, 10), 0, p1, 100, 10, 4);
+        InternetContract c2 = new InternetContract(1, LocalDate.of(2019, 11, 10),
+                LocalDate.of(2022, 11, 10),
+                1, p1, 300);
+        TVContract c3 = new TVContract(2, LocalDate.of(2019, 12, 10),
+                LocalDate.of(2022, 12, 10),
+                2, p2, new ArrayList<Channels>());
+        MyList<Contract> listContract = new MyContractList();
+        listContract.add(c1);
+        listContract.add(c2);
+        listContract.add(c3);
+
+        assertEquals(c2, listContract.get(1));
+    }
+
+    @Test
+    void size() {
+        Person p1 = new Person("Valentina", LocalDate.of(2001, 11, 18), 'w');
+        Person p2 = new Person("Alex", LocalDate.of(2000, 2, 28), 'm');
+        MobileContract c1 = new MobileContract(0,LocalDate.of(2019, 11, 10),
+                LocalDate.of(2022, 11, 10), 0, p1, 100, 10, 4);
+        InternetContract c2 = new InternetContract(1, LocalDate.of(2019, 11, 10),
+                LocalDate.of(2022, 11, 10),
+                1, p1, 300);
+        TVContract c3 = new TVContract(2, LocalDate.of(2019, 12, 10),
+                LocalDate.of(2022, 12, 10),
+                2, p2, new ArrayList<Channels>());
+        MyList<Contract> listContract = new MyContractList();
+        listContract.add(c1);
+        listContract.add(c2);
+        listContract.add(c3);
+
+        assertEquals(3, listContract.size());
+    }
+}
